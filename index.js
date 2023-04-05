1326159ae079beb3f3183a2dd4a8e5b7ec472b59
Francisco Perez <Franciscop693@gmail.com>
2023-04-05 04:54:59 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+
+function superbowlWin(array){
+    let win = array.find(item => item.result === "W")
+    if(win){
+        return win.year;
+    }else{
+        return undefined;
+    }
+}
+
